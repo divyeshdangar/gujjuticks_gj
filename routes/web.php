235fc76cb82089605ddb330349f46f590c67501b
Divyesh Dangar <divyeshdangar95@gmail.com>
2024-03-28 17:11:07 +0530

    google login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/google/redirect', [App\Http\Controllers\Auth\GoogleLoginController::class, 'redirectToGoogle'])->name('google.redirect');
+Route::get('/google/callback', [App\Http\Controllers\Auth\GoogleLoginController::class, 'handleGoogleCallback'])->name('google.callback');
+
 Route::get('/', function () {
     return view('welcome');
 });
