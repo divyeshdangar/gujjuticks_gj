a4a9fec90a6ee5b8c7c9cac9fcb4e79e7074bd04
Divyesh Dangar <divyeshdangar95@gmail.com>
2024-04-26 06:58:22 +0530

    Location model seeder factory added

---
+++ database/migrations/2024_04_24_174018_create_locations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('name_gj')->nullable();
+            $table->foreignId('parent_id')->nullable();
+            $table->text('description')->nullable();
+            $table->text('description_gj')->nullable();
+            $table->longText('details')->nullable();
+            $table->longText('details_gj')->nullable();
+            $table->string('url');
+            $table->string('lgd');
+            $table->timestamps();
+            $table->foreign('parent_id')->references('id')->on('locations');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+};
