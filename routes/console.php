9eb2b4d665a15f221fd85f93c50dfe895036d3aa
Divyesh Dangar <divyeshdangar95@gmail.com>
2025-08-27 10:19:10 +0530

    schedule update

--- routes/console.php
+++ routes/console.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Schedule;
  
 Schedule::command('app:import-google-alert-news')->everyFiveMinutes();
+Schedule::command('app:generate-posts-command')->everyFourMinutes();
